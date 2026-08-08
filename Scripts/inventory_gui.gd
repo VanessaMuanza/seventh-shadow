af72119db0f261287d3f7ca56c00269d56a15b6c
Vanessa <VanessaDolia.Muanza@edu.gbjb.ch>
2026-08-08 21:52:08 +0200

    système d'inventaire amélioré

--- Scripts/inventory_gui.gd
+++ Scripts/inventory_gui.gd
@@ -6,17 +6,37 @@ signal closed
 var isOpen: bool = false
 
 @onready var inventory = preload("res://inventory/playerInventory.tres")
+@onready var ItemStackGuiClass = preload("res://Scenes/itemsStackGui.tscn")
 @onready var slots: Array = $NinePatchRect/GridContainer.get_children()
 
+var itemInHand: ItemStackGui
+
 func _ready() -> void:
+	connectSlots()
 	inventory.updated.connect(update)
 	update()
+	
+func connectSlots():
+	for i in range(slots.size()):
+		var slot = slots[i]
+		slot.index = i
+		
+		var callable = Callable(onSlotClicked)
+		callable = callable.bind(slot)
+		slot.pressed.connect(callable)
 
 func update():
-	for i in range(min(inventory.items.size(), slots.size())):
-		slots[i].update(inventory.items[i])
+	for i in range(min(inventory.slots.size(), slots.size())):
+		var inventorySlot: InventorySlot = inventory.slots[i]
+		if !inventorySlot.item: continue
+		
+		var itemStackGui: ItemStackGui = slots[i].itemStackGui
+		if !itemStackGui:
+			itemStackGui = ItemStackGuiClass.instantiate()
+			slots[i].insert(itemStackGui)
 
-
+		itemStackGui.inventorySlot = inventorySlot
+		itemStackGui.update()
 
 func open():
 	visible = true
@@ -27,3 +47,41 @@ func close():
 	visible = false
 	isOpen = false
 	closed.emit()
+	
+func onSlotClicked(slot):
+	if slot.isEmpty() && itemInHand:
+		insertItemInSlot(slot)
+		return
+	if !itemInHand:
+		takeItemFromSlot(slot)
+
+
+
+func takeItemFromSlot(slot):
+	itemInHand = slot.takeitem()
+	add_child(itemInHand)
+	updateitemInHand()
+	
+func insertItemInSlot(slot):
+	var item = itemInHand
+	
+	remove_child(itemInHand)
+	itemInHand = null
+	
+	slot.insert(item)
+
+func updateitemInHand():
+	if !itemInHand: return
+	itemInHand.global_position = get_global_mouse_position() - itemInHand.size / 2
+	
+	
+func _input(event):
+	updateitemInHand()
+
+
+func _on_closed() -> void:
+	get_tree().paused = false
+
+
+func _on_opened() -> void:
+	get_tree().paused = true
